4b2ad98baccd826b3798debc578dba2652430044
Pablo Arce Cascante <josepa_arce@hotmail.com>
2016-03-12 15:03:11 -0600

    Rest Structure added
    *Api subdomain added
    *Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  namespace :api, path: '/', constraints: { subdomain: 'api' } do
+    resources :users
+    resources :sessions
+    resources :products
+    resources :transfers
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
