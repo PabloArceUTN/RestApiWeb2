b8117e2fe04b56f4b7af611e9d2f537e3e4c7b15
Pablo Arce Cascante <josepa_arce@hotmail.com>
2016-03-26 03:00:19 -0600

    LOGIN

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,19 @@
 class SessionsController < ApplicationController
   def index
-    render :text => "Hola esto es el login"
+    user = User.find_by(username: params[:username])
+    #check for an existent user and execute the password authentication
+    if user && user.authenticate(params[:password])
+      user.update_attribute(:valid_up, DateTime.now)
+      render json: "[{\"token\": \"#{user.token}\"}]", status: 200
+    else
+      render json: '[{"error":"Submited information does not match"}]'
+    end
   end
 
   def logout
-    render :text => "Hola esto es el logout"
+    user = User.find_by(username: params[:username])
+    @new_token = User.get_new_token
+    #user.update_attribute(:token, @new_token )
+  render :text => @new_token
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ActiveRecord::Base
   has_secure_password
   #This should be executed before a new user record will stored in the DB
   before_create :set_token
-  #This executed before user record will saved
-  #before_save :encrypt_password
-  #This executed after user record will saved
-  #after_save :clear_password
   #I don f**kin  know what is this shit for
   #attr_accessor :password_digest
   #Username validations: Unique!, Can't be blank, 4 is the minimun characters.
@@ -15,8 +11,15 @@ class User < ActiveRecord::Base
   validates :password, :presence => true, :length => { :in => 8..20 }
   #Fistname validations: Can't be blank
   validates :firstname, :presence => true
-  private
 
+  #Get new token
+  def self.get_new_token
+    User.new.instance_eval do
+      return generate_auth_token
+    end
+  end
+
+  private
   def set_token
     return if token.present?
     self.token = generate_auth_token
@@ -25,16 +28,4 @@ class User < ActiveRecord::Base
   def generate_auth_token
     SecureRandom.uuid.gsub(/\-/,'')
   end
-
-  # def encrypt_password
-  #   if password.present?
-  #     salt = BCrypt::Engine.generate_salt
-  #     password = BCrypt::Engine.hash_secret(password, salt)
-  #   end
-  # end
-  #
-  # def clear_password
-  #   self.password = nil
-  # end
-
 end
