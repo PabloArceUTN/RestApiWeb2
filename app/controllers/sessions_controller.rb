bfbf49c9b06929022d22c00d828ce27bcbe7db71
Pablo Arce Cascante <josepa_arce@hotmail.com>
2016-03-26 23:35:28 -0600

    time increment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     #check for an existent user and execute the password authentication
     if user && user.authenticate(params[:password])
-      user.update_attribute(:valid_up, DateTime.now)
+      user.update_attribute(:valid_up, DateTime.now + 30.minutes)
       render json: "[{\"token\": \"#{user.token}\"}]", status: 200
     else
       render json: '[{"error":"Submited information does not match"}]'
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
   def logout
     user = User.find_by(username: params[:username])
     @new_token = User.get_new_token
-    #user.update_attribute(:token, @new_token )
-  render :text => @new_token
+    user.update_attribute(:token, @new_token )
+  render :text => @new_toke
   end
 end
